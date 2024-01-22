1e743c08e0606196d46f5288b3d207fe087d16f6
aqtagon <ahmed.danawi@gmail.com>
2024-01-21 22:24:03 -0800

    Added to complete index.js

--- models/index.js
+++ models/index.js
@@ -5,3 +5,29 @@ const Comment = require('./comment');
 User.hasMany(Post, {
     foreignKey: 'user_id',
 });
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onabort: 'SET NULL',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+module.exports = { Post, User, Comment };
