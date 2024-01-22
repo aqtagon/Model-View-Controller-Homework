d96aa1665477fdffa6aef28c36af76854797307f
aqtagon <ahmed.danawi@gmail.com>
2024-01-22 14:40:27 -0800

    Added complete code yo server js

--- server.js
+++ server.js
@@ -6,3 +6,32 @@ const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+});
