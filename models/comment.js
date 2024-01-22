ea959baab6fd33d52a4742e709bbd96d28988ada
aqtagon <ahmed.danawi@gmail.com>
2024-01-21 21:30:07 -0800

    Added to comments.js

--- models/comment.js
+++ models/comment.js
@@ -26,3 +26,28 @@ class Post extends Model {
     } 
 }
 
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    
