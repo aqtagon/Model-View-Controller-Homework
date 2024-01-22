639bc75a148f18ae4a241545a3674240fc45abe6
aqtagon <ahmed.danawi@gmail.com>
2024-01-21 21:03:53 -0800

    Added to home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection.js');
+const { Post, User, Comment } = require('./../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
