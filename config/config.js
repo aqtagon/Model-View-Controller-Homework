f70587f841f372fcc2dd7c272edd00810644fdc9
aqtagon <ahmed.danawi@gmail.com>
2024-01-22 18:10:13 -0800

    Update database configuration for Heroku

--- config/config.js
+++ config/config.js
@@ -17,6 +17,13 @@ module.exports = {
   },
   "production": {
     "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
-};
+    "dialect": "mysql",
+    "dialectOptions": {
+        "ssl": {
+          "require": true,
+          "rejectUnauthorized": false
+        }
+      }
+    }
+  };
+
